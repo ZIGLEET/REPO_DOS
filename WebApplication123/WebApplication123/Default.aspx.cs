8bac0c28ab37f4a677ca242522861f0b6c5279bb
ZIGLEET <jbaca@zigleet.com>
2017-01-06 14:33:08 -0500

    Update Default.aspx.cs

--- WebApplication123/WebApplication123/Default.aspx.cs
+++ WebApplication123/WebApplication123/Default.aspx.cs
@@ -11,7 +11,7 @@ namespace WebApplication123
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string nombre = "";
+            string nombre = "JORGE";
         }
     }
 }
